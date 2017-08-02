42f8533f90b3254ba93dacb7831079b2f598b8f6
Sarinaphat <kaew.knn@gmail.com>
2017-08-02 04:22:47 -0700

    edit SlaRenderProjectInfo

--- src/containers/SlaRenderProjectInfo.js
+++ src/containers/SlaRenderProjectInfo.js
@@ -19,18 +19,33 @@ export default class SlaRenderProjectInfo extends Component{
           <div className="row">
               <div className="col-xs-12 col-sm-4 col-md-4 col-lg-4">
                   <div>Contract</div>
-                  <div>Project Name</div>
-                  <div>End User</div>
-                  <div>MA Start</div>
-                  <div>MA End</div>
-                  </div>
-                      <div className="col-xs-12 col-sm-8 col-md-8 col-lg-8">
+              </div>
+              <div className="col-xs-12 col-sm-8 col-md-8 col-lg-8">
                       <div>{this.props.contract}</div>
-                      <div>{this.props.project}</div>
-                      <div>{this.props.endUser}</div>
-                      <div>{this.props.ma_start}</div>
-                      <div>{this.props.ma_end}</div>
-
+              </div>
+              <div className="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                <div>Project Name</div>
+              </div>
+              <div className="col-xs-12 col-sm-8 col-md-8 col-lg-8">
+                <div>{this.props.project}</div>
+              </div>
+              <div className="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                <div>End User</div>
+              </div>
+              <div className="col-xs-12 col-sm-8 col-md-8 col-lg-8">
+                <div>{this.props.endUser}</div>
+              </div>
+              <div className="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                <div>MA Start</div>
+              </div>
+              <div className="col-xs-12 col-sm-8 col-md-8 col-lg-8">
+                <div>{this.props.ma_start}</div>
+              </div>
+              <div className="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                <div>MA End</div>
+              </div>
+              <div className="col-xs-12 col-sm-8 col-md-8 col-lg-8">
+                <div>{this.props.ma_end}</div>
               </div>
           </div>
       </div>
