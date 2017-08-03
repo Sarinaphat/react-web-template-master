ee5920c540a0b34a097043039cd61798e5a111f5
Sarinaphat <kaew.knn@gmail.com>
2017-08-02 21:27:03 -0700

    edit SlaNumberTeamSupport

--- src/containers/SlaNumberTeamSupport.js
+++ src/containers/SlaNumberTeamSupport.js
@@ -16,11 +16,11 @@ export default class SlaNumberTeamSupport extends Component {
       <div>
         <div className="row">
           <div className="col-xs-12 col-sm-4 col-md-4 col-lg-4 m-b-15 " style={{textAlign:'right', margin:'auto'}}>
-            <h3>NumberTeamSupport</h3>
+            <h3>Number Team Support</h3>
           </div>
           <div className="col-xs-12 col-sm-8 col-md-8 col-lg-8 m-b-15 ">
             <div className="row">
-              <div className="col-xs-12 col-sm-2 col-md-2 col-lg-2 m-b-15 ">
+              <div className="col-xs-12 col-sm-4 col-md-4 col-lg-4 m-b-15 ">
                 <TextField value={NumberTeamSupport}
                   onChange={(e)=>this.setState({NumberTeamSupport:e.target.value})}
                   floatingLabelText="Number Team Service" hintText="จำนวนทีม Support" style={{width:'100%'}} type="number" />
